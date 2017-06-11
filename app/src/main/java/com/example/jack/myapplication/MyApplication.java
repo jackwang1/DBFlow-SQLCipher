6509bcc8798c3c2bc7502387a97c9a476f2061de
Jack Wang <jack@atheerlabs.com>
2017-06-11 15:43:35 -0700

    @trevjonez's patch using fix in dev snapshot

--- app/src/main/java/com/example/jack/myapplication/MyApplication.java
+++ app/src/main/java/com/example/jack/myapplication/MyApplication.java
@@ -3,12 +3,12 @@ package com.example.jack.myapplication;
 import android.app.Application;
 
 import com.example.mylibrary.MyLibraryDatabase;
+import com.raizlabs.android.dbflow.config.AppGeneratedDatabaseHolder;
 import com.raizlabs.android.dbflow.config.DatabaseConfig;
 import com.raizlabs.android.dbflow.config.DatabaseDefinition;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
-import com.raizlabs.android.dbflow.config.MainGeneratedDatabaseHolder;
-import com.raizlabs.android.dbflow.config.ModuleGeneratedDatabaseHolder;
+import com.raizlabs.android.dbflow.config.MyLibraryGeneratedDatabaseHolder;
 import com.raizlabs.android.dbflow.structure.database.DatabaseHelperListener;
 import com.raizlabs.android.dbflow.structure.database.OpenHelper;
 
@@ -17,8 +17,8 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         FlowManager.init(new FlowConfig.Builder(this)
-                .addDatabaseHolder(MainGeneratedDatabaseHolder.class)
-                .addDatabaseHolder(ModuleGeneratedDatabaseHolder.class)
+                .addDatabaseHolder(AppGeneratedDatabaseHolder.class)
+                .addDatabaseHolder(MyLibraryGeneratedDatabaseHolder.class)
                 .addDatabaseConfig(getConfig(MyDatabase.class))
                 .addDatabaseConfig(getConfig(MyLibraryDatabase.class))
                 .openDatabasesOnInit(true)
